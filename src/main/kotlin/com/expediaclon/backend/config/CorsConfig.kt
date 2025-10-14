a1959f44688793c8e118b070191afdc2ea1f59a9
MCavieres <maca.c.91@gmail.com>
2025-10-14 17:37:11 -0300

    cors configuration is added

---
+++ src/main/kotlin/com/expediaclon/backend/config/CorsConfig.kt
@@ -0,0 +1,16 @@
+package com.expediaclon.backend.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**") // aplica a todas las rutas
+            .allowedOrigins("http://localhost:5173") // el origen de tu frontend
+            .allowedMethods("GET", "POST","PATCH", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+    }
+}
